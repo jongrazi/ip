2cb5a3c0db4a920cfb0e82c9c66439863417d9b4
jongrazi <jonathangraziano11@gmail.com>
2025-08-21 21:55:54 +0800

    Implemented Echo Function

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
         String separator = "_".repeat(60);
@@ -6,7 +8,25 @@ public class Friday {
         System.out.println("Welcome back sir, I am F.R.I.D.A.Y");
         System.out.println("What can I do for you sir?");
         System.out.println(separator);
-        System.out.println("Goodbye sir");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            //if bye then go to goodbye block
+            if (command.trim().equalsIgnoreCase("bye")) {
+                System.out.println(separator);
+                System.out.println("Goodbye sir");
+                System.out.println(separator);
+                break;
+            }
+
+            //to echo the text input
+            System.out.println(separator);
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
+
+        scanner.close();
     }
 }
