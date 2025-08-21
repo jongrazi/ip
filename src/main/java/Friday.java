c3e6e6f457466c3ebcb047afc7a7fc907dbdb010
jongrazi <jonathangraziano11@gmail.com>
2025-08-21 21:44:52 +0800

    Implemented skeletal chatbot Friday

---
+++ src/main/java/Friday.java
@@ -0,0 +1,12 @@
+public class Friday {
+    public static void main(String[] args) {
+        String separator = "_".repeat(60);
+
+        System.out.println(separator);
+        System.out.println("Welcome back sir, I am F.R.I.D.A.Y");
+        System.out.println("What can I do for you sir?");
+        System.out.println(separator);
+        System.out.println("Goodbye sir");
+        System.out.println(separator);
+    }
+}
