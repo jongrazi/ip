22786a921d1493eea57d0ae0e50f5d69bb3f0d29
jongrazi <jonathangraziano11@gmail.com>
2025-08-21 23:38:01 +0800

    Implemented mark as done function

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -11,7 +11,7 @@ public class Friday {
         System.out.println("What can I do for you sir?");
         System.out.println(separator);
 
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -20,27 +20,49 @@ public class Friday {
             if (command.trim().equalsIgnoreCase("bye")) {
                 //if bye then go to goodbye block
                 System.out.println(separator);
-                System.out.println("Affirmative, goodnight sir.");
+                System.out.println("Affirmative, goodnight boss.");
                 System.out.println(separator);
                 break;
 
             } else if (command.equalsIgnoreCase("list")) {
                 //if list then show the stored list
                 System.out.println(separator);
+                System.out.println("Affirmative, loading tasks...");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + tasks.get(i));
             }
 
             System.out.println(separator);
 
-        } else {
-            //else store all the text
-            tasks.add(command);
-            System.out.println(separator);
-            System.out.println("added: " + command);
-            System.out.println(separator);
-        }
+            } else if (command.startsWith("mark ")) {
+                int index = Integer.parseInt(command.substring(5)) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    Task t = tasks.get(index);
+                    t.markAsDone();
+                    System.out.println(separator);
+                    System.out.println("Okay boss, I have marked this task as done:");
+                    System.out.println(" " + t);
+                    System.out.println(separator);
+                }
 
+            } else if (command.startsWith("unmark ")) {
+                int index = Integer.parseInt(command.substring(7)) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    Task t = tasks.get(index);
+                    t.markAsNotDone();
+                    System.out.println(separator);
+                    System.out.println("Okay boss, I have marked this task as not done yet:");
+                    System.out.println(" " + t);
+                    System.out.println(separator);
+                }
+
+            } else {
+                Task t = new Task(command);
+                tasks.add(t);
+                System.out.println(separator);
+                System.out.println("added: " + command);
+                System.out.println(separator);
+            }
         }
         scanner.close();
     }
