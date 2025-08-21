eaad1b2e66727fa7e88cde325c671885c60c0928
jongrazi <jonathangraziano11@gmail.com>
2025-08-21 22:08:10 +0800

    Implemented list function and changed Friday lines

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Friday {
@@ -5,28 +7,41 @@ public class Friday {
         String separator = "_".repeat(60);
 
         System.out.println(separator);
-        System.out.println("Welcome back sir, I am F.R.I.D.A.Y");
+        System.out.println("It's good to have you back sir, I am F.R.I.D.A.Y");
         System.out.println("What can I do for you sir?");
         System.out.println(separator);
+
+        List<String> tasks = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
 
-            //if bye then go to goodbye block
             if (command.trim().equalsIgnoreCase("bye")) {
+                //if bye then go to goodbye block
                 System.out.println(separator);
-                System.out.println("Goodbye sir");
+                System.out.println("Affirmative, goodnight sir.");
                 System.out.println(separator);
                 break;
+
+            } else if (command.equalsIgnoreCase("list")) {
+                //if list then show the stored list
+                System.out.println(separator);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
             }
 
-            //to echo the text input
             System.out.println(separator);
-            System.out.println(" " + command);
+
+        } else {
+            //else store all the text
+            tasks.add(command);
+            System.out.println(separator);
+            System.out.println("added: " + command);
             System.out.println(separator);
         }
 
+        }
         scanner.close();
     }
 }
