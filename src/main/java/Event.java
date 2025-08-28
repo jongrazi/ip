558f6d6712bffa05caf09bd83fba9b30005a0ee7
jongrazi <jonathangraziano11@gmail.com>
2025-08-28 14:42:26 +0800

    Implemented enums

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,6 +10,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return TaskType.EVENT.icon() + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
