2874aba35f79ab7ad2b611926e5fc005595edb82
jongrazi <jonathangraziano11@gmail.com>
2025-09-18 19:15:47 +0800

    Add improved GUI, with user and chatbot avatars

--- src/main/java/friday/Main.java
+++ src/main/java/friday/Main.java
@@ -1,19 +1,50 @@
 package friday;
 
+import friday.storage.Storage;
+import friday.task.TaskList;
+import friday.ui.MainWindow;
+import friday.ui.Ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * GUI entrypoint that mirrors CLI behaviour and welcome.
+ */
 public class Main extends Application {
+
+    private Ui ui;
+    private Storage storage;
+    private TaskList tasks;
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
-        AnchorPane root = loader.load();
+        ui = new Ui();
+        Path savePath = Paths.get("data", "duke.txt");   // same file as CLI
+        storage = new Storage(savePath);
+        tasks = new TaskList(storage.load());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
         stage.setTitle("Friday");
-        stage.setScene(new Scene(root));
+
+        MainWindow controller = fxmlLoader.getController();
+        controller.init(ui, storage, tasks);
+
         stage.show();
+
+
+        String welcome =
+                "It's good to have you back sir, I am F.R.I.D.A.Y\n"
+                + "What can I do for you sir?\n"
+                + "Type 'help' to learn more\n";
+
+        controller.showFridayMessage(welcome); // shows in GUI
     }
 
     public static void main(String[] args) {
--- src/main/java/friday/ui/Ui.java
+++ src/main/java/friday/ui/Ui.java
@@ -5,6 +5,7 @@ public class Ui {
         System.out.println(separator);
         System.out.println("It's good to have you back sir, I am F.R.I.D.A.Y");
         System.out.println("What can I do for you sir?");
+        System.out.println("Type 'help' to learn more");
         System.out.println(separator);
     }
 }
